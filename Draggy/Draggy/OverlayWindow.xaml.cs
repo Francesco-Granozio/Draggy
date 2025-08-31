50da2737cade2ef07ee04e90ffa60bcd11be7397
Francesco Granozio <59570638+Francesco-Granozio@users.noreply.github.com>
2025-08-31 18:48:09 +0200

    Aggiungi ridimensionamento finestra e migliora UI
    Modifiche al file `OverlayWindow.xaml` per aumentare le dimensioni della finestra a 300x400, aggiungere un bordo ridimensionabile e migliorare l'aspetto dei pulsanti e delle etichette. Aggiornati i colori di sfondo e dei bordi per un'estetica migliore.
    
    Nel file `OverlayWindow.xaml.cs`, implementate nuove funzioni per gestire il ridimensionamento della finestra e migliorate le funzionalità di drag-and-drop con aggiornamenti sulla trasparenza e il colore del bordo.

--- Draggy/Draggy/OverlayWindow.xaml.cs
+++ Draggy/Draggy/OverlayWindow.xaml.cs
@@ -30,6 +30,60 @@ namespace Draggy
             System.Diagnostics.Debug.WriteLine($"ItemsControl DataContext: {ItemsControlFiles.DataContext != null}");
         }
 
+        private void Header_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Permette di trascinare la finestra cliccando sull'header
+            this.DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Minimizza la finestra
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private bool _isResizing = false;
+        private Point _resizeStartPoint;
+        private Size _resizeStartSize;
+
+        private void ResizeBorder_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                _isResizing = true;
+                _resizeStartPoint = e.GetPosition(this);
+                _resizeStartSize = new Size(this.Width, this.Height);
+                this.CaptureMouse();
+                e.Handled = true;
+            }
+        }
+
+        private void ResizeBorder_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (_isResizing)
+            {
+                Point currentPoint = e.GetPosition(this);
+                double deltaX = currentPoint.X - _resizeStartPoint.X;
+                double deltaY = currentPoint.Y - _resizeStartPoint.Y;
+
+                double newWidth = Math.Max(200, _resizeStartSize.Width + deltaX);
+                double newHeight = Math.Max(150, _resizeStartSize.Height + deltaY);
+
+                this.Width = newWidth;
+                this.Height = newHeight;
+            }
+        }
+
+        protected override void OnMouseLeftButtonUp(System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_isResizing)
+            {
+                _isResizing = false;
+                this.ReleaseMouseCapture();
+            }
+            base.OnMouseLeftButtonUp(e);
+        }
+
         private void Grid_DragEnter(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -37,9 +91,9 @@ namespace Draggy
                 e.Effects = DragDropEffects.Copy;
                 System.Diagnostics.Debug.WriteLine("DragEnter: File rilevato");
                 
-                // Aumenta la trasparenza quando inizia il drag
-                MainBorder.Background = new SolidColorBrush(Color.FromArgb(180, 255, 255, 255));
-                MainBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(200, 76, 175, 80));
+                // Aumenta la trasparenza quando inizia il drag - stile Apple
+                MainBorder.Background = new SolidColorBrush(Color.FromArgb(200, 255, 255, 255));
+                MainBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(180, 0, 122, 255)); // Apple Blue
             }
             else
             {
@@ -64,8 +118,8 @@ namespace Draggy
         private void Grid_DragLeave(object sender, DragEventArgs e)
         {
             // Ripristina la trasparenza normale quando il drag esce dalla finestra
-            MainBorder.Background = new SolidColorBrush(Color.FromArgb(32, 255, 255, 255));
-            MainBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(64, 204, 204, 204));
+            MainBorder.Background = new SolidColorBrush(Color.FromArgb(21, 255, 255, 255)); // #15FFFFFF
+            MainBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(48, 204, 204, 204)); // #30CCCCCC
         }
 
         private async void Grid_Drop(object sender, DragEventArgs e)
@@ -74,8 +128,8 @@ namespace Draggy
             e.Handled = true;
             
             // Ripristina la trasparenza normale dopo il drop
-            MainBorder.Background = new SolidColorBrush(Color.FromArgb(32, 255, 255, 255));
-            MainBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(64, 204, 204, 204));
+            MainBorder.Background = new SolidColorBrush(Color.FromArgb(21, 255, 255, 255)); // #15FFFFFF
+            MainBorder.BorderBrush = new SolidColorBrush(Color.FromArgb(48, 204, 204, 204)); // #30CCCCCC
             
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
